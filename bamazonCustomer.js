66d9dea7fd6a882fa8cf0ca4ab5f99ad61be843c
Mark Reed <markdreed512@gmail.com>
2019-03-10 15:28:56 -0500

    update DB and display cost functions added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,44 +13,67 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId + "\n");
 });
 
-var query = "SELECT * FROM bamazonDB";
+var query = "SELECT * FROM products";
 
-connection.query("SELECT * FROM products", function (err, result) {
-
+connection.query(query, function (err, result) {
     for (var i = 0; i < result.length; i++) {
         console.log(
             `Item ID: ${result[i].item_id}. **${result[i].product_name}** in Department: ${result[i].department_name}  Price: $${result[i].price}  In Stock: ${result[i].stock_quantity}`);
     }
-    
+    runInquirer()
 });
-connection.end();
-inquirer.prompt([{
-    name: "id",
-    type: "input",
-    message: "Enter item ID"
-},
-{
-    name: "units",
-    type: "input",
-    message: "How many would you like to purchase?"
-}]).then(function (answers) {
-    connection.query("SELECT * FROM products", function (err, result) {
-        console.log(result)
-        //if the stock quantity at id = input id is < the inputUnits, error 
-        // if (){
+function runInquirer() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "Enter item ID"
+        },
+        {
+            name: "units",
+            type: "input",
+            message: "How many would you like to purchase?"
+        }
+    ]).then(function (answers) {
+        connection.query("SELECT * FROM products", function (err, result) {
+            var inputId = parseInt(answers.id);
+            var availStock = parseInt(result[inputId - 1].stock_quantity);
+            var unitsPurchased = parseInt(answers.units);
+            if (availStock < unitsPurchased) {
+                console.log("insufficient Quantity!\n");
+                runInquirer();
+            } else {
+                updateDb(availStock, unitsPurchased, inputId)
+            }
 
-        // }
-
-    
-});
-connection.end();
 
 
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+        });
 
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-// However, if your store does have enough of the product, you should fulfill the customer's order.
 
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
-})
+
+    })
+}
+function updateDb(avail, purchased, id) {
+    connection.query("UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: avail - purchased
+            },
+            {
+                item_id: id
+            }
+        ], function (err, res) {
+
+        });
+    displayTotalCost(id ,purchased);
+    connection.end();
+}
+
+function displayTotalCost(itemId, num){
+    connection.query("SELECT * FROM products", function(err, result){
+        //cost = num * item price
+        var cost = num * result[itemId - 1].price;
+        console.log("Total Cost: $", cost)
+    })
+}
